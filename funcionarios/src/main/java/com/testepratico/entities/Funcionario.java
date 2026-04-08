b44f9f104c35c1f4e05832cc97ffa4bfcbff8821
Pedro Gabriel <58262161+pedrold10@users.noreply.github.com>
2026-04-08 14:38:01 -0300

    Inicializando classe de funcionario

---
+++ funcionarios/src/main/java/com/testepratico/entities/Funcionario.java
@@ -0,0 +1,22 @@
+package com.testepratico.entities;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+
+@Getter
+@Setter
+public class Funcionario extends Pessoa{
+    public Funcionario(String nome, LocalDate dataNascimento, BigDecimal salario, String funcao) {
+        super(nome, dataNascimento);
+        this.salario = salario;
+        this.funcao = funcao;
+    }
+
+    private BigDecimal salario;
+
+    private String funcao;
+}
